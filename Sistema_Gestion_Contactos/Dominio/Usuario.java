969a460e30ff8d6da3fb704d995882ec88a05d58
Oscar-Cast <zs24013043@estudiantes.uv.mx>
2025-12-06 17:07:27 -0600

    Refactor Usuario class to include password hashing
    Added overloaded constructor for password hashing and updated existing constructor to use it.

--- Sistema_Gestion_Contactos/Dominio/Usuario.java
+++ Sistema_Gestion_Contactos/Dominio/Usuario.java
@@ -11,12 +11,20 @@ public class Usuario implements Serializable {
     private String passwordHash;
     private String email;
 //Constructores
-    public Usuario(String nombreCompleto, String nombreUsuario, String passwordHash, String email) {
+    public Usuario(String nombreCompleto, String nombreUsuario, String passwordPlano, String email) {
+        this.nombreCompleto = nombreCompleto;
+        this.nombreUsuario = nombreUsuario;
+        this.passwordHash = UtileriaSeguridad.generarHash(passwordPlano);
+        this.email = email;
+    }
+
+    public Usuario(String nombreCompleto, String nombreUsuario, String passwordHash) {
         this.nombreCompleto = nombreCompleto;
         this.nombreUsuario = nombreUsuario;
         this.passwordHash = passwordHash;
-        this.email = email;
+        this.email = "";
     }
+    
     public String getNombreUsuario() {
         return nombreUsuario;
     }
@@ -46,6 +54,7 @@ public class Usuario implements Serializable {
                 ", email='" + email + '\'' +
                 '}';
     }
+
 
 
 
