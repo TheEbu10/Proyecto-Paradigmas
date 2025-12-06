118fa259d8dc8d51f2e44f07adabe0986175a09b
Valero12 <valeroj219@gmail.com>
2025-12-06 16:36:21 -0600

    Update Usuario.java

---
+++ Sistema_Gestion_Contactos/Dominio/Usuario.java
@@ -0,0 +1,30 @@
+package Dominio;
+public class Usuario {
+    private String nombreCompleto;
+    private String nombreUsuario;
+    private String passwordHash;
+    private String email;
+
+    public Usuario(String nombreCompleto, String nombreUsuario, String passwordHash, String email) {
+        this.nombreCompleto = nombreCompleto;
+        this.nombreUsuario = nombreUsuario;
+        this.passwordHash = passwordHash;
+        this.email = email;
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getNombreCompleto() {
+        retunr nombreCompleto;
+    }
+
+    public String getpasswordhash() {
+        return passwordHash;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    
