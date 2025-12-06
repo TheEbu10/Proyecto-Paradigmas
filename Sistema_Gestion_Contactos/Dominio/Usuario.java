7ee06da4d41357bd23c0547ee9b154b7567fcad4
Oscar-Cast <zs24013043@estudiantes.uv.mx>
2025-12-06 16:58:26 -0600

    Reformat Usuario.java with consistent indentation

--- Sistema_Gestion_Contactos/Dominio/Usuario.java
+++ Sistema_Gestion_Contactos/Dominio/Usuario.java
@@ -2,6 +2,7 @@ package Dominio;
 
 import Persistencia.UtileriaSeguridad;
 import java.io.Serializable;
+
 public class Usuario implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -44,6 +45,7 @@ public class Usuario implements Serializable {
                 ", email='" + email + '\'' +
                 '}';
     }
+
 
 
 
