63b41455b3b3962231fccb80fe0ec38a1782cc6d
TheEbu10 <zS24013055@estudiantes.uv.mx>
2025-12-06 16:17:05 -0600

    Actualizacion

--- Sistema_Gestion_Contactos/Persistencia/solicitudCompartir.java
+++ Sistema_Gestion_Contactos/Persistencia/solicitudCompartir.java
@@ -4,13 +4,13 @@ public class solicitudCompartir {
     private String nombreReceptor;
     private String estadoSolicitud; 
 
-    public solicitudCompartir(String nombreSolicitante, String nombreReceptor) {
+    public solicitudCompartir(String nombreSolicitante, String nombreReceptor) { // Constructor
         this.nombreSolicitante = nombreSolicitante;
         this.nombreReceptor = nombreReceptor;
         this.estadoSolicitud = "Pendiente"; // Estado inicial
     }
 
-    public String getNombreSolicitante() {
+    public String getNombreSolicitante() { // obtencion de datos para la solicitud 
         return nombreSolicitante;
     }
     public String getNombreReceptor() {
